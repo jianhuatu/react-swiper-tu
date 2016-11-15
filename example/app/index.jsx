e0db5c22070f8686b048806b7ae8dfd9ca8f03b2
jianhuatu <tujianhua123@gmail.com>
2016-11-15 17:24:15 +0800

    修改文件
    修改文件

--- example/app/index.jsx
+++ example/app/index.jsx
@@ -2,45 +2,32 @@ import React from "../../node_modules/react";
 import ReactDom from "../../node_modules/react-dom";
 import SwiperComponent from "../../index";
 
+var swiperOption = {
+	direction: "vertical"
+}
+
 ReactDom.render(
-	(<SwiperComponent>
+	(<SwiperComponent swiperOption={swiperOption}>
 		<div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
-			<div>111</div>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+			<div>5</div>
+			<div>6</div>
+			<div>7</div>
+			<div>8</div>
+			<div>9</div>
+			<div>5</div>
+			<div>6</div>
+			<div>7</div>
+			<div>8</div>
+			<div>9</div>
+			<div>5</div>
+			<div>6</div>
+			<div>7</div>
+			<div>8</div>
+			<div>9</div>
 		</div>
 	</SwiperComponent>),
 	document.getElementById("example")
